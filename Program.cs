17cd6318fe5e18b4b14e3a60d1defc50aeb69ea7
Natália Serrão <nataliaserrao45@gmail.com>
2023-08-01 13:29:04 -0400

    feature: implementacao classe abstrata

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Nokia celularNokia = new Nokia(numero:"1234", modelo:"Z-Flip90", imei:"123456789", memoria:"128");
+celularNokia.Ligar();
+celularNokia.ReceberLigacao();
+celularNokia.InstalarAplicativo("Twitter");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Iphone celularIphone = new Iphone(numero: "9876", modelo:"14ProMax", imei:"4326578984", memoria:"64");
+celularIphone.Ligar();
+celularIphone.ReceberLigacao();
+celularIphone.InstalarAplicativo("Whatsapp");
